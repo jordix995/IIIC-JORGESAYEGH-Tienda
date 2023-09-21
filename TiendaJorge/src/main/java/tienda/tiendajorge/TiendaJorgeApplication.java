1e1bab053a32e04af69bc39ed85600e154862b99
Jorge Sayegh <jorgelias995@gmail.com>
2023-09-20 19:25:29 -0600

    Clase2.0
    Primer commit de la clase 2

---
+++ TiendaJorge/src/main/java/tienda/tiendajorge/TiendaJorgeApplication.java
@@ -0,0 +1,13 @@
+package tienda.tiendajorge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaJorgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TiendaJorgeApplication.class, args);
+    }
+
+}
---
+++ TiendaJorge/src/test/java/tienda/tiendajorge/TiendaJorgeApplicationTests.java
@@ -0,0 +1,13 @@
+package tienda.tiendajorge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaJorgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
